a3fd6c582d0530168baa8e1a0049f13cfe108a02
sschoedel <sschoedel@vt.edu>
2020-02-16 03:57:14 -0500

    new initMap script

--- static/script.js
+++ static/script.js
@@ -1,28 +1,3 @@
-function initMap() {
-  $.ajax({
-    type : "POST",
-    dataType : "json",
-    contentType : "application/json",
-    url : "/curCords",
-    statusCode : {
-      200 : function(cords) {
-        console.log("Successfully displayed map with coords");
-        console.log(cords);
-        // TODO: shove google api here and use cords.cords
-
-        var bothCoords = {lat : cords.X, lng : cords.Y};
-        var zoomAmount = 8;
-
-        var map = new google.maps.Map(document.getElementById('map'),
-                                      {center : bothCoords, zoom : zoomAmount});
-
-        var marker = new google.maps.Marker({position : bothCoords, map : map});
-      }
-    }
-
-  });
-}
-
 function sendData() {
   // The data we should send.
   data = {
